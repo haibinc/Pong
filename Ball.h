d157517eb30f3425b31c2777f105e34bf2bcb5c2
haibinc <haibinc143@gmail.com>
2022-10-09 03:48:52 -0700

    https://github.com/haibinc/BouncingBall.git

--- Ball.h
+++ Ball.h
@@ -1,27 +1,16 @@
 //
-// Created by Haibin Cao on 10/7/22.
+// Created by Haibin Cao on 10/8/22.
 //
 
 #ifndef BOUNCINGBALL_BALL_H
 #define BOUNCINGBALL_BALL_H
-#include <iostream>
-#include <SFML/Graphics.hpp>
+
+
 
 class Ball {
-public:
-    Ball();
-    void run();
-    void bounce(sf::Vector2i window);
-private:
-    void processEvents();
-    void update();
-    void render();
-private:
-    double speedX = 0.2;
-    double speedY = 0.2;
-    sf::RenderWindow window;
-    sf::CircleShape ball;
+
 };
+
 
 
 #endif //BOUNCINGBALL_BALL_H
---
+++ Game.h
@@ -0,0 +1,28 @@
+//
+// Created by Haibin Cao on 10/7/22.
+//
+
+#ifndef BOUNCINGBALL_BALL_H
+#define BOUNCINGBALL_BALL_H
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Ball {
+public:
+    Ball();
+    void run();
+    void bounce(sf::Vector2i window);
+    void setSpeed(double speedX, double speedY);
+private:
+    void processEvents();
+    void update();
+    void render();
+private:
+    double speedX = 0.2;
+    double speedY = 0.2;
+    sf::RenderWindow window;
+    sf::CircleShape ball;
+};
+
+
+#endif //BOUNCINGBALL_BALL_H
