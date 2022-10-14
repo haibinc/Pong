dfc5f16f96eca6565af3bd160c07435d17ad7b74
haibinc <haibinc143@gmail.com>
2022-10-13 19:58:07 -0700

    first commit

---
+++ Paddle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Haibin Cao on 10/10/22.
+//
+
+#include "paddle.h"
---
+++ Paddle.h
@@ -0,0 +1,14 @@
+//
+// Created by Haibin Cao on 10/10/22.
+//
+
+#ifndef BOUNCINGBALL_PADDLE_H
+#define BOUNCINGBALL_PADDLE_H
+
+
+class paddle {
+
+};
+
+
+#endif //BOUNCINGBALL_PADDLE_H
